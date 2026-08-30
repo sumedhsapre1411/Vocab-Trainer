1e9421eaea65f297b8936acd3bbd9856bcd024c3
sumedhsapre1411 <sumedhsapre1411@gmail.com>
2026-08-30 19:41:21 +0530

    Update asset list in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE="vocab-trainer-pwa-v1";
-const ASSETS=["./","./vocab-trainer.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
